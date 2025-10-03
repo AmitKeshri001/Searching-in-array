d0677dd3362e7f968b83baf2e5332c637b1dea51
AmitKeshri001 <AmitKeshri001>
2025-10-03 23:47:26 +0530

    add a Question First And Last Ocuurance BinarySearch

---
+++ First_and_Last_Ocuurance_BinarySearch.java
@@ -0,0 +1,64 @@
+package com.amit;
+
+import java.util.Arrays;
+
+public class First_and_last_position_Leetcode_05 {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 7, 7, 8, 8, 10};
+        int target = 7;
+        int[] a = searchrange(arr, target);
+        System.out.println(Arrays.toString(a));
+    }
+
+    static int[] searchrange(int[] arr,int target)
+    {
+        int[] ans = {-1,-1};
+
+        int start = search(arr,target,true);
+        int end = search(arr,target,false);
+
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+
+    static int search(int[] arr, int target, boolean b)
+    {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+
+            if(target>arr[mid])
+            {
+                start = mid+1;
+
+            }
+            else if(target<arr[mid])
+            {
+                end = mid - 1;
+
+            }
+            else
+            {
+                ans = mid;
+
+                if(b)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid+1;
+                }
+            }
+
+        }
+        return ans;
+    }
+//
+}
